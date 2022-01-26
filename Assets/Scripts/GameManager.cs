4b1a896f709658f994f07261c4b814e1e1c1b704
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-01-25 22:04:19 -0300

    feat(LevelComplete): added Level complete and one more level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,13 @@ public class GameManager : MonoBehaviour
 
     public float restartDelay = 1f;
 
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
     public void GameOver()
     {
         if (isGameEnded)
